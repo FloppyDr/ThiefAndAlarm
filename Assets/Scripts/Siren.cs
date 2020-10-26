dcd792df5f61358c956e15b6bec4874cfb45ca1e
FloppyDr <DimaAlexandrov@yandex.ru>
2020-10-26 03:57:58 +0300

    Changed siren script

--- Assets/Scripts/Siren.cs
+++ Assets/Scripts/Siren.cs
@@ -7,36 +7,40 @@ public class Siren : MonoBehaviour
 {
     [SerializeField] private AudioSource _audio;
 
-    public void Play()
+    public void TurnOffSiren()
     {
-        StartCoroutine(ChangeVolume());
+        StopAllCoroutines();
+        StartCoroutine(FadeIn());
     }
 
-    private IEnumerator ChangeVolume()
+    public void TurnOnSiren()
     {
-        int steps = 15;
-        var delay = new WaitForSeconds(0.01f);
+        StopAllCoroutines();
+        StartCoroutine(FadeOut());
+    }
 
-        if (_audio.volume == 0)
-        {
-            for (int i = 0; i < steps; i++)
+    private IEnumerator FadeOut()
+    {
+        var delay = new WaitForSeconds(0.005f);
+        
+            while (_audio.volume < 0.9)
             {
-                _audio.volume = Mathf.Lerp(_audio.volume,1,i*Time.deltaTime);
+                _audio.volume = Mathf.Lerp(_audio.volume, 1, Time.deltaTime);
                 yield return delay;
             }
-
             _audio.volume = 1;
+    }
 
-        }
-        else if (_audio.volume == 1)
+    private IEnumerator FadeIn()
+    {
+        var delay = new WaitForSeconds(0.005f);
+
+        while (_audio.volume > 0.01)
         {
-            for (int i = 0; i < steps; i++)
-            {
-                _audio.volume = Mathf.Lerp(_audio.volume, 0, i * Time.deltaTime);
-                yield return delay;
-            }
-
-            _audio.volume = 0;
+            _audio.volume = Mathf.Lerp(_audio.volume, 0, Time.deltaTime);
+            yield return delay;
         }
+        _audio.volume = 0;
+
     }
 }
